972b2d6241814ec9e3b726592134b0ae4c2d33ff
iz2k <ibon@zalbide.com>
2024-10-06 15:31:04 +0200

    imports fixed

--- examples/simple_example.py
+++ examples/simple_example.py
@@ -2,14 +2,12 @@ import logging
 import time
 
 # To use from source
-from src.nurapi import NUR, NurTagCount, NurTagData, NurInventoryResponse, NurModuleSetup, NUR_MODULESETUP_FLAGS, \
-    NurReaderInfo, NurDeviceCaps
-from src.nurapi.enums import SETUP_RX_DEC, SETUP_LINK_FREQ, NurBank, SETUP_RF_PROFILE, NUR_ERRORCODES
+from src.nurapi import NUR, NurModuleSetup, NUR_MODULESETUP_FLAGS
+from src.nurapi.enums import SETUP_RX_DEC, SETUP_LINK_FREQ, NurBank
 
 # To use from installed package
-#from nurapi import NUR, NurTagCount, NurTagData, NurInventoryResponse, NurModuleSetup, NUR_MODULESETUP_FLAGS, \
-#    NurReaderInfo, NurDeviceCaps
-#from nurapi.enums import SETUP_RX_DEC, SETUP_LINK_FREQ, NurBank, SETUP_RX_PROFILE
+# from nurapi import NUR, NurModuleSetup, NUR_MODULESETUP_FLAGS
+# from nurapi.enums import SETUP_RX_DEC, SETUP_LINK_FREQ, NurBank
 
 logging.basicConfig(level=logging.DEBUG)
 
